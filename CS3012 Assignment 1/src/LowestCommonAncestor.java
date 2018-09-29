daa9c9c89b83630d8a6c158b4f53b352b7cf3af1
Tiernan <Tiernan@192.168.0.157>
2018-09-29 19:12:53 +0100

    Getting started

---
+++ CS3012 Assignment 1/src/LowestCommonAncestor.java
@@ -0,0 +1,16 @@
+public class LowestCommonAncestor 
+{
+
+}
+
+class Node
+{
+	int nodeNumber;
+	Node left;
+	Node right;
+	Node(int value)
+	{
+		nodeNumber=value;
+		left = right = null;
+	}
+}
---
+++ CS3012 Assignment 1/src/LowestCommonAncestorTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LowestCommonAncestorTest {
+
+	@Test
+	void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
