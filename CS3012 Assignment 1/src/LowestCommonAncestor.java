64997587b2d70625306a65d2289cc76ea658ce84
Tiernan <Tiernan@192.168.0.157>
2018-09-29 20:09:43 +0100

    Main code

--- CS3012 Assignment 1/src/LowestCommonAncestor.java
+++ CS3012 Assignment 1/src/LowestCommonAncestor.java
@@ -1,6 +1,14 @@
 public class LowestCommonAncestor 
 {
-
+	Node findLowestCommonAncestor(Node n, int n1, int n2) 
+	{
+		if(n==null) return null; //returns null if tree is empty
+		if(n.equals(n1) || n.equals(n2)) return n; //returns n if either node is the root
+		Node leftLowestCommonAncestor = findLowestCommonAncestor(n.left,n1,n2); //checks if there nodes in left subtree
+		Node rightLowestCommonAncestor = findLowestCommonAncestor(n.right,n1,n2); //checks if there are nodes in right subtree
+		if(leftLowestCommonAncestor!=null && rightLowestCommonAncestor!=null) return n;
+		return (leftLowestCommonAncestor!=null)?leftLowestCommonAncestor:rightLowestCommonAncestor; //returns Lowest Common Ancestor of subtrees
+	}
 }
 
 class Node
